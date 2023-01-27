a7211c1b4e31172dc01c73136d70b356e5ab4a09
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2023-01-27 17:32:41 -0500

    added operators, punctuation and comments

--- COMP 442/src/Lexer.java
+++ COMP 442/src/Lexer.java
@@ -28,9 +28,9 @@ public class Lexer {
                     }
                     // Case of Equal Token
                     else if ((char) nextChar == ' ') {
-                        //create a token for lastcharread and '=' and reset lastcharsread
+                        //validate and create a token for lastcharread and '=' and reset lastcharsread
                     } else {
-                        //create a token for lastcharread and then token for '='
+                        //validate and create a token for lastcharread and then token for '='
                         // and clear string for lastcharsread and set it to the nextChar
                     }
                 } else if ((char) currentPointerChar == ':') {
@@ -40,8 +40,7 @@ public class Lexer {
                     if ((char) nextChar == ':') {
                         // check if lastcharsread is not empty and make its token
                         // and then make a token for ':'
-                    }
-                    else if ((char) nextChar == ' ') {
+                    } else if ((char) nextChar == ' ') {
                         //create a token for lastcharread and ':' and reset lastcharsread
                     } else {
                         //create a token for lastcharread and then token for ':'
@@ -72,21 +71,20 @@ public class Lexer {
                     if ((char) nextChar == '=') {
                         // check if lastcharsread is not empty and make its token
                         // and then make a token for '>='
-                    }
-                    else if ((char) nextChar == ' ') {
+                    } else if ((char) nextChar == ' ') {
                         //create a token for lastcharread and '>' and reset lastcharsread
                     } else {
                         // create a token for lastcharread and then token for '>'
-                        // and clear string for lastcharsread and set it to the nextChar
+                        // and clear string for lastcharsread and set it to the nextChar if not space
                     }
                 } else if ((char) currentPointerChar == '/') {
                     nextChar = fileInputStream.read();
 
                     // Case of Inline Comment
                     if ((char) nextChar == '/') {
-                       lastCharsRead = "//";
-                        while((nextChar = fileInputStream.read())!= '\n' && (nextChar = fileInputStream.read())!= '\r'){
-                            lastCharsRead += (char)nextChar;
+                        lastCharsRead = "//";
+                        while ((nextChar = fileInputStream.read()) != '\n' && (nextChar) != '\r') {
+                            lastCharsRead += (char) nextChar;
                         }
                         //create token for inline
                     }
@@ -94,21 +92,21 @@ public class Lexer {
                     else if ((char) nextChar == '*') {
                         lastCharsRead = "/*";
                         boolean checkIfBlockEnded = false;
-                        while(!checkIfBlockEnded){
+                        while (!checkIfBlockEnded) {
                             nextChar = fileInputStream.read();
                             // finds a *
-                            if((char)nextChar =='*'){
+                            if ((char) nextChar == '*') {
                                 lastCharsRead += "*";
                                 nextChar = fileInputStream.read();
                                 // finds the '*/'
-                                if((char)nextChar =='/'){
+                                if ((char) nextChar == '/') {
                                     // found end of comment
                                     lastCharsRead += "/";
                                     //make token for comment and add to arraylist
                                     break;
                                 }
                             }
-                            lastCharsRead += (char)nextChar;
+                            lastCharsRead += (char) nextChar;
                         }
                     }
                     // Case of division
@@ -118,9 +116,67 @@ public class Lexer {
                         // create a token for lastcharread and then token for '/'
                         // and clear string for lastcharsread and set it to the nextChar
                     }
+                } else if ((char) currentPointerChar == '.') {
+
+                    boolean isAId = validateId(lastCharsRead);
+
+                    if (isAId == true) {
+                        //make token for lastCharRead and token for '.' and reset last char read
+                    } else {
+                        lastCharsRead += '.';
+                        while (((nextChar = fileInputStream.read()) >= 48 && nextChar <= 57) || (char) nextChar == 'e' || (char) nextChar == '+' || (char) nextChar == '-') {
+                            lastCharsRead += (char) nextChar;
+                        }
+                        boolean isAFloat = validateFloat(lastCharsRead);
+                        if (isAFloat == true) {
+                            //create token for float
+                            // reset lastcharsread to empty, but add back the nextchar just read if it is not a space
+                            lastCharsRead = "" + nextChar;
+
+                        } else {
+                            // error case invalid period
+                        }
+                    }
+                } else if ((char) currentPointerChar == '*') {
+                    // validate and create token for lastcharsread and then create token for *
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == '+') {
+                    // validate and create token for lastcharsread and then create token for +
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == '-') {
+                    // validate and create token for lastcharsread and then create token for -
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == ',') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == ';') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == '(') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == ')') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                }else if ((char) currentPointerChar == '}') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == '{') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                }else if ((char) currentPointerChar == ']') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
+                } else if ((char) currentPointerChar == '[') {
+                    // validate and create token for lastcharsread and then create token for ,
+                    // reset lastcharsread to "" empty
                 }
 
 
+                if (lastCharsRead.equals(";") || lastCharsRead.equals(")") || lastCharsRead.equals("]") || lastCharsRead.equals("}")) {
+                    // do if stateents and make a token
+                    // reset lastcharsread to "" empty
+                }
 //                lastCharsRead += (char)currentPointerChar;
 //                previousChar = currentPointerChar;
             }
@@ -136,4 +192,25 @@ public class Lexer {
         //while loop restarts
 
     }
+
+    public boolean validateFloat(String lastCharRead) {
+        Boolean b = false;
+        return b;
+    }
+
+    public boolean validateInteger(String lastCharRead) {
+        Boolean b = false;
+        return b;
+    }
+
+    public boolean validateId(String lastCharRead) {
+        Boolean b = false;
+        return b;
+    }
+
+    public boolean validateReservedWord(String lastCharRead) {
+        Boolean b = false;
+        return b;
+    }
+
 }
