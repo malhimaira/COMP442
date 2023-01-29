23e2f8157dcb2fad34038eccf2a3bcc32237e6b7
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2023-01-29 02:44:42 -0500

    commit for driver file

--- COMP 442/src/LexDriver.java
+++ COMP 442/src/LexDriver.java
@@ -1,3 +1,5 @@
+import com.sun.source.util.SourcePositions;
+
 import java.io.FileInputStream;
 
 public class LexDriver {
@@ -7,6 +9,12 @@ public class LexDriver {
         try {
             FileInputStream fileInputStream = new FileInputStream("/Users/mairamalhi/IdeaProjects/COMP442/COMP 442/example-bubblesort.src");
             Lexer lexer = new Lexer(fileInputStream);
+            Token token;
+            while((token = lexer.getNextToken()) != null){
+                System.out.println(token.getLexeme());
+            }
+
+            lexer.getNextToken();
         } catch (Exception e) {
             System.out.print(e.getMessage());
         }
